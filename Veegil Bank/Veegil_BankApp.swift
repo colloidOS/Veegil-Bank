5d155ecabcdd249d9775f1d24588e7c70f1d7050
colloidOS <nnagoziemcollin@gmail.com>
2023-11-29 11:57:48 +0100

    Initial Commit

---
+++ Veegil Bank/Veegil_BankApp.swift
@@ -0,0 +1,17 @@
+//
+//  Veegil_BankApp.swift
+//  Veegil Bank
+//
+//  Created by Macintosh HD on 29/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct Veegil_BankApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
